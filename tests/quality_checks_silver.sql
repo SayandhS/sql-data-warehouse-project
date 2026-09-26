48a4654a16cbcb1900cb8b047b61e964ee6eb6c3
SayandhS <132703551+SayandhS@users.noreply.github.com>
2026-09-26 13:45:52 +0530

    Update quality_checks_silver.sql

--- tests/quality_checks_silver.sql
+++ tests/quality_checks_silver.sql
@@ -77,14 +77,14 @@ FROM silver.crm_prd_info;
 -- Check for invalid dates
 SELECT
 NULLIF(sls_order_dt,0) sls_order_dt
-FROM bronze.crm_sales_details
+FROM silver.crm_sales_details
 WHERE sls_order_dt <=0 
 	OR LENGTH(CAST(sls_order_dt AS TEXT))!=8 
 	OR sls_order_dt > 20500101 
 	OR sls_order_dt < 19000101;
 
 -- Check for Invalid Orders 
-SELECT * FROM bronze.crm_sales_details
+SELECT * FROM silver.crm_sales_details
 WHERE sls_order_dt>sls_ship_dt OR sls_ship_dt>sls_due_dt;
 
 -- Check for Sales Busines Validity
@@ -102,7 +102,7 @@ SELECT DISTINCT
 	   			THEN sls_sales / NULLIF(sls_quantity,0)
 			ELSE sls_price
 		END AS sls_price
-FROM bronze.crm_sales_details
+FROM silver.crm_sales_details
 WHERE sls_sales!= sls_quantity * sls_price 
 OR sls_sales IS NULL
 OR sls_quantity IS NULL
@@ -118,7 +118,7 @@ ORDER BY sls_sales, sls_quantity, sls_price;
 -- ---------------------------------------------------------------------------
 -- Check out-of-range dates
 SELECT DISTINCT bdate
-	FROM bronze.erp_cust_az12
+	FROM silver.erp_cust_az12
 	WHERE bdate<='1924-01-01' OR bdate>CURRENT_TIMESTAMP;
 
 -- Data standardization and Normalisation
@@ -128,7 +128,7 @@ SELECT gen,
 		 WHEN UPPER(TRIM(gen)) IN ('M','MALE') THEN 'Male'
 		 ELSE 'n/a'
 	END AS gen
-	FROM bronze.erp_cust_az12;
+	FROM silver.erp_cust_az12;
 
 
 -- ---------------------------------------------------------------------------
@@ -141,12 +141,12 @@ SELECT DISTINCT cntry,
 					 WHEN TRIM(cntry)='' OR cntry IS NULL THEN 'n/a'
 					 ELSE TRIM(cntry)
 				END AS cntry_new			
-FROM bronze.erp_loc_a101
+FROM silver.erp_loc_a101
 ORDER BY cntry;
 
 -- ---------------------------------------------------------------------------
 --For ERP_PX_CAT_G1V2
 -- ---------------------------------------------------------------------------
 -- Check for unwanted spaces
-SELECT * FROM bronze.erp_px_cat_g1v2
+SELECT * FROM silver.erp_px_cat_g1v2
 WHERE TRIM(cat)!=cat OR TRIM(sub_cat)!=sub_cat OR TRIM(maintainence)!=maintainence;
